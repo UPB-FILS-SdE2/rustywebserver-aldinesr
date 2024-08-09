bf1b74b862be5d9383a26c3c58da5a5bc174c750
Bader al dine srour <110973421+aldinesr@users.noreply.github.com>
2024-08-09 13:17:38 +0000

    test listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
+use tokio::stream;
+
+
 fn main() {
-    println!("Test ");
+    // println!("Test ");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+        println!("connected");
+    }
 } 
